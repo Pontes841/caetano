6e0fc863e656f7655a3ae4fd4111a409abac76cb
Pontes841 <98322224+Pontes841@users.noreply.github.com>
2023-11-16 08:45:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const qrcode = require('qrcode');
 const fileUpload = require('express-fileupload');
 const moment = require('moment');
-const port = 8008;
+const port = 8004;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
@@ -18,9 +18,9 @@ const nodeCron = require('node-cron');
 const createConnection = async () => {
     return await mysql.createConnection({
         host: '212.1.208.101',
-        user: 'u896627913_dinizpalmeira',
-        password: 'Felipe.91118825',
-        database: 'u896627913_dinizpalmeira'
+        user: 'u896627913_luciano01',
+        password: 'Felipe@91118825',
+        database: 'u896627913_luciano01'
     });
 }
 
@@ -693,8 +693,8 @@ client.on('ready', async () => {
                         console.log('URL da mensagemvd:', agendamento.mensagemvd);
                         try {
                             const media = await MessageMedia.fromUrl(agendamento.mensagemvd);
-                            const linkURL = 'https://www.instagram.com/oticasdiniz.pdi/'; // Replace this with your desired link URL
-                            const textBelowImage = 'Olá! Que tal nos seguir no Instagram ? Temos um conteúdo incrível que você vai adorar! Basta clicar no link abaixo.Se já nos segue, ignore essa mensagem.';
+                            const linkURL = 'https://g.page/r/CaWcuer6OFEdEBM/review/'; // Replace this with your desired link URL
+                            const textBelowImage = 'Seu feedback é importante para a Óticas Diniz RO. Poste uma avaliação no nosso perfil.';
                             const linkText = 'Clique aqui para avaliar'; // Replace this with the text you want to display for the link
 
                             const caption = `${textBelowImage}\n\n${linkText}: ${linkURL}`;
