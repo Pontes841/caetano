5263a81413550e118d09bcc2c27d514e968a8087
Pontes841 <98322224+Pontes841@users.noreply.github.com>
2024-03-15 09:51:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const qrcode = require('qrcode');
 const fileUpload = require('express-fileupload');
 const moment = require('moment');
-const port = 8006;
+const port = 8001;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
@@ -18,9 +18,9 @@ const nodeCron = require('node-cron');
 const createConnection = async () => {
     return await mysql.createConnection({
         host: '212.1.208.101',
-        user: 'u896627913_luciano03',
-        password: 'Felipe@91118825',
-        database: 'u896627913_luciano03'
+        user: 'u896627913_santana',
+        password: 'Felipe.91118825',
+        database: 'u896627913_santana'
     });
 }
 
@@ -298,6 +298,18 @@ const updateStatastaxa = async (id) => {
     }
 };
 
+// FunÃ§Ã£o para atualizar o renovaÃ§Ã£o dos oculos (controle taxa)
+const updateStatasmensagem = async (id) => {
+    try {
+        const connection = await createConnection();
+        const query = 'UPDATE mensagens SET status = "enviado" WHERE id = ?';
+        const [result] = await connection.execute(query, [id]);
+        return result.affectedRows > 0;
+    } catch (error) {
+        console.error('Erro ao atualizar o status:', error);
+        return false;
+    }
+};
 
 // FunÃ§Ã£o para obter os registros de agendamento do banco de dados
 const agendamentoZDG0 = async () => {
@@ -557,6 +569,18 @@ const agendamentoZDG21 = async () => {
     }
 };
 
+// RenovaÃ§Ã£o dos oculos (controle taxa)
+
+const agendamentoZDG22 = async () => {
+    try {
+        const connection = await createConnection();
+        const [rows] = await connection.execute('SELECT * FROM mensagens WHERE status IS NULL OR status = ""');
+        return rows;
+    } catch (error) {
+        console.error('Erro ao obter os registros de agendamento:', error);
+        return [];
+    }
+};
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -641,6 +665,7 @@ client.on('ready', async () => {
             const agendamentosgarantiafilefi = await agendamentoZDG19();
             const agendamentospapdia = await agendamentoZDG20();
             const agendamentostaxa = await agendamentoZDG21();
+            const agendamentosmensagem = await agendamentoZDG22();
 
             const hoje = new Date();
 
@@ -674,39 +699,40 @@ client.on('ready', async () => {
 
 
 
-                for (const agendamento of agendamentosSolicitacao) {
-                    if (agendamento.data_inclusao && agendamento.data_inclusao <= hoje && !agendamento.enviado) {
-                        // Marcar o agendamento como enviado
-                        agendamento.enviado = true;
+            for (const agendamento of agendamentosSolicitacao) {
+                if (agendamento.data_inclusao && agendamento.data_inclusao <= hoje && !agendamento.enviado) {
+                    // Marcar o agendamento como enviado
+                    agendamento.enviado = true;
 
-                        if (agendamento.nome !== '') {
-                            client.sendMessage(agendamento.fone + '@c.us', agendamento.nome);
-                        }
+                    if (agendamento.nome !== '') {
+                        client.sendMessage(agendamento.fone + '@c.us', agendamento.nome);
+                    }
 
-                        if (agendamento.mensagemvd && agendamento.mensagemvd !== '') {
-                            console.log('URL da mensagemvd:', agendamento.mensagemvd);
-                            try {
-                                const media = await MessageMedia.fromUrl(agendamento.mensagemvd);
-                                const linkURL = 'https://g.page/r/CYHjj8Vb6zLgEBM/review/'; // Replace this with your desired link URL
-                                const textBelowImage = 'Seu feedback é importante para a Óticas Diniz RO. Poste uma avaliação no nosso perfil.';
-                                const linkText = 'Clique aqui para avaliar'; // Replace this with the text you want to display for the link
+                    if (agendamento.mensagemvd && agendamento.mensagemvd !== '') {
+                        console.log('URL da mensagemvd:', agendamento.mensagemvd);
+                        try {
+                            const media = await MessageMedia.fromUrl(agendamento.mensagemvd);
+                            const linkURL = 'https://www.instagram.com/oticasdinizsantanadoipanema/'; // Replace this with your desired link URL
+                            const textBelowImage = 'Olá! Que tal nos seguir no Instagram ? Temos um conteúdo incrível que você vai adorar! Basta clicar no link abaixo.Se já nos segue, ignore essa mensagem.';
+                            const linkText = 'Clique aqui para avaliar'; // Replace this with the text you want to display for the link
 
-                                const caption = `${textBelowImage}\n\n${linkText}: ${linkURL}`;
+                            const caption = `${textBelowImage}\n\n${linkText}: ${linkURL}`;
 
-                                client.sendMessage(agendamento.fone + '@c.us', media, { caption });
-                            } catch (error) {
-                                console.error('Erro ao obter a mensagemvd:', error);
-                            }
-                        }
-
-                        const success = await updateStatusvd(agendamento.id);
-                        if (success) {
-                            console.log('BOT-ZDG - Mensagem ID: ' + agendamento.id + ' - statusvd atualizado para "enviado"');
-                        } else {
-                            console.log('BOT-ZDG - Falha ao atualizar o statusvd da mensagem ID: ' + agendamento.id);
+                            client.sendMessage(agendamento.fone + '@c.us', media, { caption });
+                        } catch (error) {
+                            console.error('Erro ao obter a mensagemvd:', error);
                         }
                     }
+
+                    const success = await updateStatusvd(agendamento.id);
+                    if (success) {
+                        console.log('BOT-ZDG - Mensagem ID: ' + agendamento.id + ' - statusvd atualizado para "enviado"');
+                    } else {
+                        console.log('BOT-ZDG - Falha ao atualizar o statusvd da mensagem ID: ' + agendamento.id);
+                    }
                 }
+            }
+
 
             for (const agendamento of agendamentosFinalizacao) {
                 if (agendamento.data_finalizacao && agendamento.data_finalizacao <= hoje && !agendamento.enviado) {
@@ -1282,6 +1308,37 @@ client.on('ready', async () => {
                 }
             }
 
+            for (const agendamento of agendamentosmensagem) {
+                if (agendamento.data && agendamento.data <= hoje && !agendamento.enviado) {
+                    // Marcar o agendamento como enviado
+                    agendamento.enviado = true;
+
+                    if (agendamento.imagem && agendamento.imagem !== '') {
+                        console.log('URL da imagem:', agendamento.imagem);
+                        try {
+                            const media = await MessageMedia.fromUrl(agendamento.imagem);
+                            await client.sendMessage(agendamento.fone + '@c.us', media, { caption: '' });
+                        } catch (error) {
+                            console.error('Erro ao obter a imagem:', error);
+                        }
+                    }
+                    
+                    if (agendamento.mensagem !== '') {
+                        try {
+                            await client.sendMessage(agendamento.fone + '@c.us', agendamento.mensagem);
+                        } catch (error) {
+                            console.error('Erro ao enviar a mensagem:', error);
+                        }
+                    }
+                    
+                    const success = await updateStatasmensagem(agendamento.id);
+                    if (success) {
+                        console.log('BOT-ZDG - Mensagem ID: ' + agendamento.id + ' - status atualizado para "enviado"');
+                    } else {
+                        console.log('BOT-ZDG - Falha ao atualizar o status da imagem ID: ' + agendamento.id);
+                    }
+                }
+            }
 
         } catch (error) {
             console.error('Erro na tarefa agendada:', error);
